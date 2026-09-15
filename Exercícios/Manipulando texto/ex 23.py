357d355f1488d325f1db2401f3be6cfe0111eb29
Jaeferson Araujo <jaeferson.dev@gmail.com>
2026-09-15 16:31:42 -0300

    Atividades

--- Exercícios/Manipulando texto/ex 23.py
+++ Exercícios/Manipulando texto/ex 23.py
@@ -5,7 +5,7 @@ u = n // 1 % 10
 d = n // 10 % 10
 c = n // 100 % 10
 m = n // 1000 % 10
-print(f"Analizando o número {n} ")
+print(f"Analisando o número {n} ")
 print(f"Unidade: {u}")
 print(f"Dezena: {d}")
 print(f"Centena: {c}")
--- Exercícios/Manipulando texto/ex 24.py
+++ Exercícios/Manipulando texto/ex 24.py
@@ -1,4 +1,4 @@
 # Crie um programa que leia o nome de uma cidade diga se ela começou ou não com o nome "Santo".
 
-cid = input("Qual cidade voçê nasceu? ").strip()
+cid = input("Qual cidade você nasceu? ").strip()
 print(cid[:5].upper() == 'SANTO')
